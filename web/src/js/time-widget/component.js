1c9005b4528bcf5065c24add52bbcf606eac5a28
joshlgrossman <josh.l.grossman@gmail.com>
2017-12-17 11:44:33 -0600

    component examples

---
+++ web/src/js/time-widget/component.js
@@ -0,0 +1,19 @@
+module.exports = Vue.component('time-widget', {
+  template: require('./template.html'),
+  data: () => ({ hours: '00', minutes: '00', seconds: '00' }),
+  methods: {
+    time() {
+      const currTime = moment();
+
+      this.hours = currTime.format('hh');
+      this.minutes = currTime.format('mm');
+      this.seconds = currTime.format('ss');
+    }
+  },
+  mounted() {
+    this.interval = setInterval(this.time, 1000);
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
+  }
+});
