91aadc1190c48c08d3287b028a15d5b09813eb07
jtrobinson1993@gmail.com <jtrobinson1993@gmail.com>
2017-12-16 21:45:47 -0600

    prettiering code

--- gulpfile.js
+++ gulpfile.js
@@ -2,47 +2,70 @@ const path = require('path');
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const babel = require('gulp-babel');
+const prettier = require('gulp-prettier');
 const minifyJS = require('gulp-uglifyjs');
 const minifyCSS = require('gulp-clean-css');
 const minifyHTML = require('gulp-cleanhtml');
 const autoPrefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
 
-gulp.task('default', ['sass', 'js', 'html']);
+gulp.task('default', ['sass', 'format', 'js', 'html']);
 
 gulp.task('sass', () => {
-	return gulp
-	.src('./web/src/**/*.sass')
-	.pipe(sass({
-		paths:[path.join(__dirname, 'styles', 'includes')]
-	}))
-	.pipe(autoPrefixer({
-		browsers: ['last 2 versions']
-	}))
-	.pipe(minifyCSS())
-	.pipe(gulp.dest('./web/build/'));
+  return gulp
+    .src('./web/src/**/*.sass')
+    .pipe(
+      sass({
+        paths: [path.join(__dirname, 'styles', 'includes')]
+      })
+    )
+    .pipe(
+      autoPrefixer({
+        browsers: ['last 2 versions']
+      })
+    )
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./web/build/'));
+});
+
+gulp.task('format', () => {
+  return gulp
+    .src('./web/src/**/*.js')
+    .pipe(
+      prettier({
+        printWidth: 120,
+        tabWidth: 2,
+        useTabs: false,
+        singleQuote: true,
+        bracketSpacing: true,
+        semi: true
+      })
+    )
+    .pipe(gulp.dest('./web/src/'));
 });
 
 gulp.task('js', () => {
-	return gulp
-	.src('./web/src/**/*.js')
-	.pipe(babel({
-		presets: ['es2015', 'es2016']
-	}))
-	.pipe(minifyJS())
-	.pipe(concat('app.js'))
-	.pipe(gulp.dest('./web/build/js'));
+  return gulp
+    .src('./web/src/**/*.js')
+    .pipe(
+      babel({
+        presets: ['es2015', 'es2016']
+      })
+    )
+    .pipe(minifyJS())
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./web/build/js'));
 });
 
 gulp.task('html', () => {
-	return gulp
-	.src('./web/src/**/*.html')
-	.pipe(minifyHTML())
-	.pipe(gulp.dest('./web/build'))
+  return gulp
+    .src('./web/src/**/*.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('./web/build'));
 });
 
 gulp.task('watch', ['default'], () => {
-	gulp.watch('./web/src/**/*.js', ['js']);
-gulp.watch('./web/src/**/*.sass', ['sass']);
-gulp.watch('./web/src/**/*.html', ['html']);
+  gulp.watch('./web/src/**/*.js', ['js']);
+  gulp.watch('./web/src/**/*.sass', ['sass']);
+  gulp.watch('./web/src/**/*.html', ['html']);
 });
--- web/src/js/background-image.js
+++ web/src/js/background-image.js
@@ -1,33 +1,27 @@
 let backgroundImage = new Vue({
-	el: '#page-background',
-	data: {
-		photo: ''
-	},
-	methods: {
-		getRedditImage(subreddit) {
-			const vm = this;
-			axios.get(`https://www.reddit.com/r/${subreddit}/top/.json?sort=top&t=month`)
-				 .then(function (response) {
-					 console.log(response);
-					 const listing = response.data.data.children;
-					 const listingLength = listing.length;
-					 
-					 const randomPost = listing[Math.round(Math.random() * 25)];
-					 const randomImage = randomPost.data.url;
-					 
-					 vm.photo = randomImage;
-					 console.log(listing);
-					 console.log(randomImage);
-				 })
-				 .catch(function (error) {
-					 console.log(error);
-				 });
-		}
-	},
-	mounted() {
-		const subreddit = 'EarthPorn';
-		this.getRedditImage(subreddit);
-	}
+  el: '#page-background',
+  data: { photo: '' },
+  methods: {
+    getRedditImage(subreddit) {
+      axios
+        .get(`https://www.reddit.com/r/${subreddit}/top/.json?sort=top&t=month`)
+        .then(response => {
+          console.log(response);
+          const listing = response.data.data.children;
+          const listingLength = listing.length;
+
+          const randomPost = listing[Math.round(Math.random() * 25)];
+          const randomImage = randomPost.data.url;
+
+          this.photo = randomImage;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  },
+  mounted() {
+    const subreddit = 'EarthPorn';
+    this.getRedditImage(subreddit);
+  }
 });
-
-
