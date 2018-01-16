0447f12a72b34f2b2db34ec802ee1a1acd5012c5
Jarrod Robinson <jrobinson@proteus.co>
2018-01-16 16:05:39 -0600

    select timezone, store in local db. basic styles

--- web/src/js/time.js
+++ web/src/js/time.js
@@ -1,13 +1,27 @@
+const timezone = localStorage.getItem('timezone');
+
 let time = new Vue({
   el: '#time-widget',
-  data: { hours: '00', minutes: '00', seconds: '00' },
+  data: { 
+    hours: '00', 
+    minutes: '00', 
+    seconds: '00',
+    selected: timezone ? timezone : moment.tz.guess(),
+    options: moment.tz.names()
+  },
   methods: {
     time() {
-      const currTime = moment();
+      // Use computed properties instead?
+      const currTime = moment.tz(this.selected);
 
       this.hours = currTime.format('hh');
       this.minutes = currTime.format('mm');
       this.seconds = currTime.format('ss');
+    }
+  },
+  watch: {
+    selected: (option) => {
+      localStorage.setItem('timezone', option);
     }
   },
   mounted() {
